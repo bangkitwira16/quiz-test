bf4ef3c651f6c140baae6004a38783dcc8bc350e
bangkitwira <bangkitwira@gmail.com>
2025-07-16 23:52:19 +0700

    [feat-dev] update routes

--- src/routes.ts
+++ src/routes.ts
@@ -1,9 +1,7 @@
-import Home from './views/Home.vue'
-import About from './views/About.vue'
+import Quiz from './views/Quiz.vue'
 import NotFound from './views/NotFound.vue'
 
 export const routes = [
-  { path: '/', component: Home, meta: { title: 'Home' }},
-  { path: '/about', component: About, meta: { title: 'About' }},
+  { path: '/', component: Quiz, meta: { title: 'Quiz' }},
   { path: '/:pathMatch(.*)*', component: NotFound, meta: { title: 'Page not found' }},
 ]

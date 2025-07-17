98f148548fa7cd37f4c7c37481f4d9b5ae59bc2b
bangkitwira <bangkitwira@gmail.com>
2025-07-17 08:29:32 +0700

    [feat-dev] implement navigation button

--- src/views/Quiz.vue
+++ src/views/Quiz.vue
@@ -4,8 +4,8 @@
     <QuizHeader v-if="!isFinished" :nilai="nilai" :timer="timer" />
     <QuestionBox v-for="(item, index) in questionAnswer" :key="item.id" :question="item"
       :visible="index === currentQuestion" :current-selected="currentSelectedAnswer" @select-answer="handleAnswer" />
-    <button v-if="currentQuestion !== questionAnswer.length" @click="() => handleNext(timer)"
-      class="bg-blue-500 p-4 rounded-md mt-6 w-full xl:w-auto">Lanjut</button>
+    <NavigationButton v-if="currentQuestion !== questionAnswer.length" @click="() => handleNext(timer)"
+      class="bg-blue-500 p-4 rounded-md mt-6 w-full xl:w-auto">Lanjut</NavigationButton>
     <Result v-if="currentQuestion === questionAnswer.length" :nilai="nilai" :user-answer="userAnswer" />
   </section>
 </template>
@@ -19,6 +19,7 @@ import QuizHeader from '../components/QuizHeader.vue'
 import QuestionBox from '../components/QuestionBox.vue'
 import { useQuizTimer } from '../composables/useQuizTimer'
 import { useQuizLogic } from '../composables/useQuizLogic'
+import NavigationButton from '../components/NavigationButton.vue'
 
 const {
   currentQuestion,
